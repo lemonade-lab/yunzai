e2a16bb23ac1228387a4370beaa5f7e0055fef69
ningmengchongshui <ningmengchongshui@gmail.com>
2025-01-03 15:00:04 +0800

    docs: 优化过程

--- updateYaml.js
+++ updateYaml.js
@@ -4,14 +4,26 @@ const dir = join(process.cwd(), "config", "config", "bot.yaml");
 const ddir = join(process.cwd(), "config", "default_config", "bot.yaml");
 // 正则表达式匹配 skip_login: false，允许任意数量的空格
 const regex = /skip_login:\s*false/;
-if (fs.existsSync(dir)) {
+
+const update = () => {
   // 替换 skip_login: false 为 skip_login: true
   const data = fs.readFileSync(dir, "utf8");
-  console.log(data);
   const newData = data.replace(regex, "skip_login: true");
   fs.writeFileSync(dir, newData);
-} else if (fs.existsSync(ddir)) {
+};
+
+const updateDefault = () => {
   const data = fs.readFileSync(ddir, "utf8");
   const newData = data.replace(regex, "skip_login: true");
   fs.writeFileSync(ddir, newData);
+};
+
+if (fs.existsSync(dir)) {
+  update();
+} else if (fs.existsSync(ddir)) {
+  updateDefault();
+}
+
+if (fs.existsSync(dir)) {
+  update();
 }
