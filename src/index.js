181d42753f78fb086c3fa4cd94ace18a775cb778
xiejihuang <xiejihuang@baidu.com>
2025-02-21 19:06:23 +0800

    fix: app.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 export default defineChildren(() => ({
   async onCreated() {
-    await import('../app.js')
+    await import('../../app.js')
     await import('./icqq/global.js')
     console.log('alemonjs plugin created')
   }
